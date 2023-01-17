95418f15c2c7189cb4b093bd5daf9615d164d7de
Andrew-Sem <avvyayvayva@gmail.com>
2023-01-17 22:39:40 +0300

    fixed uncleared questions-answers chain

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -10,16 +10,17 @@ interface ResultProps{
 export const Result: FC<ResultProps> = ({ resetHandler, selectedLang, questionAnswerChain, setQuestionAnswerChain}) => {
     useEffect(() => {
         console.log(questionAnswerChain)
-        return () => {
-            setQuestionAnswerChain([])
-        }
     }, [])
+    const rerunTest = () => {
+        resetHandler()
+        setQuestionAnswerChain([])
+    }
     return (
         <div className={"flex flex-col items-center w-76 bg-gradient-to-r from-cyan-600 to-blue-600 px-6 py-12 rounded-xl"}>
             <div>Поздравляю, подходящий Вам язык: {selectedLang}</div>
             <button
                 className={"mt-10 px-6 py-3 bg-slate-900/50 hover:bg-slate-900/60 ease-in duration-100  rounded shadow-lg"}
-                onClick={resetHandler}
+                onClick={rerunTest}
             >
                 Пройти ещё раз
             </button>
